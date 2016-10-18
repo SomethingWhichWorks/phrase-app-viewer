de0187dcfc9cdaebfea9bb3e02c6f436c565e0fb
Mahesh Kulkarni <kulkarni.mak172@gmail.com>
2016-10-18 23:53:59 +0200

    Fixed issues with loading message

--- client/app/phrase-app/phrase-app-list/phrase-app-list.component.ts
+++ client/app/phrase-app/phrase-app-list/phrase-app-list.component.ts
@@ -7,40 +7,8 @@ import { PhraseAppService } from '../services/phrase-app.service';
 @Component({
   moduleId: module.id,
   selector: 'phrase-app-list',
-  /*templateUrl: 'phrase-app-list.component.html',*/
-  template: `
-  <div class="warning" *ngIf="loadingMessage">{{loadingMessage}}</div>
-  <div class="error" *ngIf="error">{{error}}</div>
-
-  <div *ngIf="loadingMessage !== ''">
-    <div *ngIf="selectedMessage">
-    <h2>Preview Section</h2>
-        <h2>
-          Phrase App Key :  {{selectedMessage.key | uppercase}}
-        </h2>
-        <!-- <li *ngFor="let label of selectedMessage.labels">
-            {{label}}
-        </li> -->
-        <pre>
-          {{selectedMessage.labels | json }}
-        </pre>
-        <button (click)="gotoDetail()">View Message Details</button>
-      <hr>    
-    </div>
-
-    <h2>Phrase App Keys</h2>
-    <ul class="phrase-app-keys">
-      <li *ngFor="let message of messages" (click)="onSelect(message)" [class.selected]="message === selectedMessage">
-        <span class="message-element">
-                <span class="badge">  {{message.key}}  </span> <h4> {{message.key}} </h4>
-          <br>
-        </span>
-      </li>
-    </ul>
-</div>
-
-`/*,
-  styleUrls: ['heroes.component.css']*/
+  templateUrl: 'phrase-app-list.component.html',
+  styleUrls: ['phrase-app-list.component.css']
 })
 
 export class PhraseAppListComponent implements OnInit {
@@ -56,21 +24,16 @@ export class PhraseAppListComponent implements OnInit {
   getMessages(): void {
     this.loadingMessage = 'loading phrase app keys, please wait... ';
 
-    var loadindMessageChanger = setInterval(function() {
-      this.loadingMessage = this.loadingMessage + '.'; 
-    }, 100);
-
     this.phraseAppService
       .getMessages()
       .then((messages) => {
         this.messages = messages;
         this.loadingMessage = '';
-        clearInterval(loadindMessageChanger);
       })
       .catch((error) => {
+        this.messages = [];
         this.error = error
         this.loadingMessage = '';
-        clearInterval(loadindMessageChanger);
     });
       
   }
